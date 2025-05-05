ccc943b0fe497b64527056cf5dee3e837fe1335e
Subiramaniam Pirashanth <shanth.pirashanth5@gmail.com>
2025-05-05 22:53:46 +0530

    Create index.jsx

---
+++ first/src/index.jsx
@@ -0,0 +1,9 @@
+import { createRoot } from 'react-dom/client';
+
+createRoot(document.getElementById('root')).render(
+    <ul>
+        <li>Super popular Js library</li>
+        <li>Used for building user interfaces</li>
+        <li>Component based</li>
+    </ul>
+);
